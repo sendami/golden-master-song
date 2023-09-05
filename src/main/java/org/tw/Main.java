f8d6291eb1c41964d189bf0a06a97bf88975e582
sendami luque <sendami.luque@thoughtworks.com>
2023-09-05 10:30:48 +0200

    Add She swallowed in a method

--- src/main/java/org/tw/Main.java
+++ src/main/java/org/tw/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static final String TO_CATCH_THE = " to catch the ";
     public static final String DOT_LINE_BREAK = ".\n";
     public static final String SEMICOLON_LINE_BREAK = ";\n";
+    public static final String COLON_LINE_BREAK = ",\n";
 
     public static void main(String[] args) {
         printSong();
@@ -19,37 +20,42 @@ public class Main {
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A_FLY_PERHAPS_SHE_LL_DIE +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + "spider" + SEMICOLON_LINE_BREAK +
                 "That wriggled and wiggled and tickled inside her" + DOT_LINE_BREAK +
-                SHE_SWALLOWED_THE + "spider" + TO_CATCH_THE + "fly" + SEMICOLON_LINE_BREAK +
+                predatorEatsPrey("spider", "fly", SEMICOLON_LINE_BREAK) +
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A_FLY_PERHAPS_SHE_LL_DIE +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + "bird" + SEMICOLON_LINE_BREAK +
                 "How absurd to swallow a bird" + DOT_LINE_BREAK +
-                SHE_SWALLOWED_THE + "bird" + TO_CATCH_THE + "spider,\n" +
-                SHE_SWALLOWED_THE + "spider" + TO_CATCH_THE + "fly" + SEMICOLON_LINE_BREAK +
+                predatorEatsPrey("bird", "spider", COLON_LINE_BREAK) +
+                predatorEatsPrey("spider", "fly", SEMICOLON_LINE_BREAK) +
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A_FLY_PERHAPS_SHE_LL_DIE +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + "cat" + SEMICOLON_LINE_BREAK +
                 "Fancy that to swallow a cat" + EXCLAMATION_LINE_BREAK +
-                SHE_SWALLOWED_THE + "cat" + TO_CATCH_THE + "bird,\n" +
-                SHE_SWALLOWED_THE + "bird" + TO_CATCH_THE + "spider,\n" +
-                SHE_SWALLOWED_THE + "spider" + TO_CATCH_THE + "fly" + SEMICOLON_LINE_BREAK +
+                predatorEatsPrey("cat", "bird", COLON_LINE_BREAK) +
+                predatorEatsPrey("bird", "spider", COLON_LINE_BREAK) +
+                predatorEatsPrey("spider", "fly", SEMICOLON_LINE_BREAK) +
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A_FLY_PERHAPS_SHE_LL_DIE +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + "dog" + SEMICOLON_LINE_BREAK +
                 "What a hog, to swallow a dog" + EXCLAMATION_LINE_BREAK +
-                SHE_SWALLOWED_THE + "dog" + TO_CATCH_THE + "cat,\n" +
-                SHE_SWALLOWED_THE + "cat" + TO_CATCH_THE + "bird,\n" +
-                SHE_SWALLOWED_THE + "bird" + TO_CATCH_THE + "spider,\n" +
-                SHE_SWALLOWED_THE + "spider" + TO_CATCH_THE + "fly" + SEMICOLON_LINE_BREAK +
+                predatorEatsPrey("dog", "cat", COLON_LINE_BREAK) +
+                predatorEatsPrey("cat", "bird", COLON_LINE_BREAK) +
+                predatorEatsPrey("bird", "spider", COLON_LINE_BREAK) +
+                predatorEatsPrey("spider", "fly", SEMICOLON_LINE_BREAK) +
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A_FLY_PERHAPS_SHE_LL_DIE +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + "cow" + SEMICOLON_LINE_BREAK +
                 "I don't know how she swallowed a cow" + EXCLAMATION_LINE_BREAK +
-                SHE_SWALLOWED_THE + "cow" + TO_CATCH_THE + "dog,\n" +
-                SHE_SWALLOWED_THE + "dog" + TO_CATCH_THE + "cat,\n" +
-                SHE_SWALLOWED_THE + "cat" + TO_CATCH_THE + "bird,\n" +
-                SHE_SWALLOWED_THE + "bird" + TO_CATCH_THE + "spider,\n" +
-                SHE_SWALLOWED_THE + "spider" + TO_CATCH_THE + "fly" + SEMICOLON_LINE_BREAK +
+                predatorEatsPrey("cow", "dog", COLON_LINE_BREAK) +
+                predatorEatsPrey("dog", "cat", COLON_LINE_BREAK) +
+                predatorEatsPrey("cat", "bird", COLON_LINE_BREAK) +
+                predatorEatsPrey("bird", "spider", COLON_LINE_BREAK) +
+                predatorEatsPrey("spider", "fly", SEMICOLON_LINE_BREAK) +
                 I_DON_T_KNOW_WHY_SHE_SWALLOWED_A_FLY_PERHAPS_SHE_LL_DIE +
                 THERE_WAS_AN_OLD_LADY_WHO_SWALLOWED_A + "horse.." + DOT_LINE_BREAK +
                 "...She's dead, of course!";
 
         System.out.println(song);
     }
+
+    public static String predatorEatsPrey(String predator, String prey, String endOfLine) {
+        return SHE_SWALLOWED_THE + predator + TO_CATCH_THE + prey + endOfLine;
+    }
+
 }

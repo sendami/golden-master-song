36fcddc53d320434b911365135a3a7d5725f95cb
sendami luque <sendami.luque@thoughtworks.com>
2023-09-14 22:33:09 +0200

    Refactor test for clarity

--- src/test/java/org/tw/SongGoldenMasterTest.java
+++ src/test/java/org/tw/SongGoldenMasterTest.java
@@ -11,18 +11,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SongGoldenMasterTest {
     @Test
     void checkOriginalSong() throws IOException {
-        // Generate the snapshot to compare it with the golden master
         ByteArrayOutputStream outStream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(outStream);
         System.setOut(printStream);
 
         SongGoldenMaster.writeTheSong();
 
-        // Expected string is the golden master song.txt
-        // Actual string is the snapshot generated when we call Main.writeTheSong
-        assertEquals(Files.readString(Path.of("song.txt")), outStream.toString());
+        String actualSong = outStream.toString();
+        String goldenMaster = Files.readString(Path.of("song.txt"));
+
+        assertEquals(goldenMaster, actualSong);
     }
 
+    //Method that generates the Golden Master
     public static void main(String[] args) throws IOException {
         PrintStream printStream = new PrintStream(new FileOutputStream("song.txt"));
         System.setOut(printStream);
